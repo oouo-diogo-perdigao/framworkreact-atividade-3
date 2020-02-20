dc9cd53f91594c3a9aa0a92dd7a436380bf6fd89
Diogo Passos Perdigão Britto <884806@sga.pucminas.br>
2020-02-20 19:50:50 -0300

    Atividade 3 zerada

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import { MoviesList } from "./components/MoviesList";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <MoviesList />
+    </div>
+  );
+}
---
+++ src/components/MoviesList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const MoviesList = () => {
+  return <h1>Code goes here</h1>;
+};
