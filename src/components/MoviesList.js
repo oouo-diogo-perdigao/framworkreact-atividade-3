6be54005fc2d24731bba94139e5c6e139a5c18e2
Diogo Passos Perdigão Britto <884806@sga.pucminas.br>
2020-02-27 21:53:53 -0300

    Simplificação lista

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,33 +4,24 @@ import { MoviesService } from "../services/MoviesService";
 import { Link } from "react-router-dom";
 
 export const MoviesList = () => {
-  const [filmes, setFilmes] = React.useState({});
+  const [filmes, setFilmes] = React.useState([]);
 
   React.useEffect(_ => {
     MoviesService.getPopularMovies().then(ret => {
       console.log(ret);
-      setFilmes(ret);
+      setFilmes(ret.data.results);
     });
   }, []);
 
-  function _mont(_filmes) {
-    console.log("_mont MoviesList");
-    if (Object.keys(_filmes).length) {
-      let text = [];
-      var filmesInterno = _filmes.data.results;
-      for (const film in filmesInterno) {
-        text.push(
-          <Link to={"/film/" + filmesInterno[film].id}>
-            {filmesInterno[film].title}
-          </Link>
-        );
-        text.push(<br />);
-      }
+  return (
+    <>
+      {filmes.map(f => (
+        <p key={f.id}>
+          <Link to={"/film/" + f.id}>{f.title}</Link>
+        </p>
+      ))}
+    </>
+  );
 
-      return <>{text}</>;
-    } else {
-      return <>vazio2</>;
-    }
-  }
-  return <>{_mont(filmes)}</>;
+  //   return <>{_mont(filmes)}</>;
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
-// import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
+// import { Provider } from "react-redux";
+// import { createStore } from "redux";
+// import { reducers } from "./reducers";
+
+// const store = createStore(
+//   reducers,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
 ReactDOM.render(
+  //   <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>,
+  //   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
