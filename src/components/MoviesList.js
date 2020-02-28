bc794ddae8a33e8800a9a9acd1c09d88d1204113
Diogo Passos Perdigão Britto <884806@sga.pucminas.br>
2020-02-27 21:07:30 -0300

    Exercicio 3

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,31 +1,36 @@
 import React from "react";
 import { MoviesService } from "../services/MoviesService";
 
+import { Link } from "react-router-dom";
+
 export const MoviesList = () => {
   const [filmes, setFilmes] = React.useState({});
 
   React.useEffect(_ => {
     MoviesService.getPopularMovies().then(ret => {
-      setFilmes(ret);
       console.log(ret);
+      setFilmes(ret);
     });
   }, []);
 
   function _mont(_filmes) {
-    if (Object.keys(_filmes)) {
+    console.log("_mont MoviesList");
+    if (Object.keys(_filmes).length) {
       let text = [];
-      console.log(_filmes);
-      //var filmesInterno = _filmes.data.results;
-      //   for (const film in filmesInterno) {
-      //     console.log(filmesInterno);
-      //     console.log(filmesInterno[film].title);
-      //     text.push(<span>{filmesInterno[film].title}</span>);
-      //   }
-      //return text;
+      var filmesInterno = _filmes.data.results;
+      for (const film in filmesInterno) {
+        text.push(
+          <Link to={"/film/" + filmesInterno[film].id}>
+            {filmesInterno[film].title}
+          </Link>
+        );
+        text.push(<br />);
+      }
+
+      return <>{text}</>;
     } else {
-      return <>vazio</>;
+      return <>vazio2</>;
     }
   }
-  console.log(filmes);
   return <>{_mont(filmes)}</>;
 };
